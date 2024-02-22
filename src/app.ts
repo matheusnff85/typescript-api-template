c98fa203742fec14adb306e1cfa16de250dc329b
Matheus Marinho <matheus.marinhodsp@gmail.com>
2024-02-22 11:53:33 -0300

    feat: finalizando base do template

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import * as cors from 'cors';
-import * as express from 'express';
+import cors from 'cors';
+import express from 'express';
 import errorHandler from './middlewares/errorHandler';
 import usersRouter from './routes/users.routes';
 
